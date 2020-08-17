f1e9049d04c4e3221fdab2fb677c37440cfd2ac0
Joseph Nguyen <josephsportycs@gmail.com>
2020-08-17 11:31:25 -0700

    Removed js file progress bar.

--- public/components/progress-bar.js
+++
@@ -1,9 +0,0 @@
-var time = 0
-function makeProgress() {
-  if (time < 100) {
-    time = time + 1
-    $(".progress-bar").css("width", time + "%").text(time + " %")
-  }
-  setTimeout("makeProgress()", 275)
-}
-makeProgress()
