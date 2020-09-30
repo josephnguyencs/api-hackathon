58fc11d1c616dac36ec564525c8f2781009b9a25
Joseph Nguyen <josephsportycs@gmail.com>
2020-09-30 12:25:55 -0700

    port change.

--- server/index.js
+++ server/index.js
@@ -14,6 +14,6 @@ app.get('/api', (req, res, next) => {
   .catch(err => res.status(500).json(err))
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port ' + process.env.PORT)
 })
